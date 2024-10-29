103faf2b98a81d3aa8c13422bb343150ef5e7449
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-10-30 05:04:10 +0700

    Fix bug on movie service

--- Repositories/Service/MovieService.cs
+++ Repositories/Service/MovieService.cs
@@ -1,20 +1,11 @@
 ﻿using BusinessObjects;
-using DataAccessLayers;
 using DataAccessLayers.UnitOfWork;
-using Microsoft.EntityFrameworkCore;
 using Services.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.Service
 {
-    public class MovieService(GenericRepository<Movie> eventDAO, IUnitOfWork unitOfWork) : GenericService<Movie>(unitOfWork), IMovieService
+    public class MovieService(IUnitOfWork unitOfWork) : GenericService<Movie>(unitOfWork), IMovieService
     {
-        private readonly GenericRepository<Movie> _eventDAO = eventDAO;
-        private readonly IUnitOfWork _unitOfWork;
 
         //public async Task<IEnumerable<Movie>> GetAllInclude()
         //{
