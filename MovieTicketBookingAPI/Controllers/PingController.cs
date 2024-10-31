5d81f1a9847e89618958acc6da0d7b4f03072714
Truong Hai Minh <144296136+minhtruong123A@users.noreply.github.com>
2024-10-31 13:56:31 +0700

    check server api PING

---
+++ MovieTicketBookingAPI/Controllers/PingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieTicketBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet("CheckServer")]
+        public IActionResult GetPingInfor() => Ok("PING CC DMMM");
+    }
+}
