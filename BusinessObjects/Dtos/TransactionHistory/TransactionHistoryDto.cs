fdf510a92241924c0ef8eb9059922c3dacb5d93a
Truong Hai Minh <144296136+minhtruong123A@users.noreply.github.com>
2024-11-04 14:35:36 +0700

    delete id in transactionhistorydto

--- BusinessObjects/Dtos/TransactionHistory/TransactionHistoryDto.cs
+++ BusinessObjects/Dtos/TransactionHistory/TransactionHistoryDto.cs
@@ -8,7 +8,6 @@ namespace BusinessObjects.Dtos.TransactionHistory
 {
     public class TransactionHistoryDto
     {
-        public int Id { get; set; }
         public string MovieName { get; set; } = string.Empty;
         public string? ShowDateTime {  get; set; } = string.Empty;
         public string? Room { get; set; } = string.Empty;
